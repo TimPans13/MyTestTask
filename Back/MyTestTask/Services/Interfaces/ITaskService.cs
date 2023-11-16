d52760e3d8f61dde8e4dcf9fe557882ce80504a9
Tim_Pans_13 <95716918+chort13@users.noreply.github.com>
2023-11-16 15:47:41 +0300

    add Services Interfaces

---
+++ Back/MyTestTask/Services/Interfaces/ITaskService.cs
@@ -0,0 +1,17 @@
+﻿using MyTestTask.Models;
+
+namespace MyTestTask.Services.Interfaces
+{
+    public interface ITaskService
+    {
+        Task<Contact> Get(int id);
+
+        Task<List<Contact>> GetAll();
+
+        Task<Contact> Add(Contact task);
+
+        Task Delete(int taskId);
+
+        Task Update(int taskId, Contact task);
+    }
+}
