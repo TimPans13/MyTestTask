49e87bc5a3da3bf983da5eb81cbb19dc3245bf1e
Tim_Pans_13 <95716918+chort13@users.noreply.github.com>
2023-11-16 15:48:55 +0300

    add DB Contecst

---
+++ Back/MyTestTask/Data/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyTestTask.Models;
+
+namespace MyTestTask.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Contact> Contacts { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+            if (!Contacts.Any())
+            {
+                Contacts.AddRange(
+                    new Contact { Name = "Ivan", MobilePhone = "+375332221100", JobTitle = "Senior", BirthDate = DateTime.Now.AddYears(-30) },
+                    new Contact { Name = "Lena", MobilePhone = "+375334441100", JobTitle = "Middle", BirthDate = DateTime.Now.AddYears(-25) }
+                );
+
+                SaveChanges();
+            }
+        }
+
+    }
+}
