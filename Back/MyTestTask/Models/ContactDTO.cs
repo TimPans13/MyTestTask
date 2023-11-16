b03efbd83c80b2122bf0a1cac0c2ee90bc504c40
Tim_Pans_13 <95716918+chort13@users.noreply.github.com>
2023-11-16 15:47:08 +0300

    models add

---
+++ Back/MyTestTask/Models/ContactDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace MyTestTask.Models
+{
+    public class ContactDTO
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? MobilePhone { get; set; }
+        public string? JobTitle { get; set; }
+        public DateTime BirthDate { get; set; }
+    }
+}
